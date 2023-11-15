5f7262b688ec95f782157d6634df2b9ddd821634
John Karson <casonj254@gmail.com>
2023-11-15 08:55:35 -0500

    simple shell

--- shell.c
+++ shell.c
@@ -1,189 +1,75 @@
 #include <stdio.h>
-#include <unistd.h>
 #include <stdlib.h>
-#include <string.h>
+#include <unistd.h>
 #include <sys/wait.h>
-
-extern char **environ;
-
-#define MAX_ARGUMENTS 100
-/**
- * executeCommand - executes a command with fork and execvp
- * @args: array of command-line arguments
- */
-void executeCommand(char *args[]);
-/**
- * @brief Custom implementation of strlen function.
- * @param str The input string.
- * @return The length of the string.
- */
-char *custom_strcat(const char *str1, const char *str2);
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <dirent.h>
+#include <string.h>
+#include "shell.h"
 
 /**
- * @brief Custom implementation of strlen function.
- * @param str The input string.
- * @return The length of the string.
+ * @brief Displays the shell prompt.
  */
-size_t custom_strlen(const char *str);
-
+void displayPrompt(void)
+{
+	printf("#cisfun$ ");
+	fflush(stdout);
+}
 /**
- * @brief Custom implementation of getline function.
- * @param line A pointer to the line buffer.
- * @param len A pointer to the buffer size.
- * @param stream The input stream.
- * @return The number of bytes read.
+ * @brief Executes a command using fork and exec.
+ * @param command The command to be executed.
  */
-ssize_t custom_getline(char **line, size_t *len, FILE *stream);
-
-/**
- * @brief Execute a command with fork and execvp.
- * @param args An array of command-line arguments.
- */
-void executeCommand(char *args[])
+void executeCommand(const char *command)
 {
 	pid_t pid = fork();
+	int status;
+	execlp(command, command, (char *)NULL);
+	waitpid(pid, &status, 0);
 
-	if (pid < 0)
+	if (pid == -1)
 	{
 		perror("fork");
 		exit(EXIT_FAILURE);
 	}
 	else if (pid == 0)
 	{
-		if (execvp(args[0], args) == -1)
-		{
-			perror("execvp");
-			exit(EXIT_FAILURE);
-		}
-	}
-	else
-	{
-		wait(NULL);
-		free(args);
-	}
-}
-/**
- * @brief Custom implementation of getline function.
- * @param line A pointer to the line buffer.
- * @param len A pointer to the buffer size.
- * @param stream The input stream.
- * @return The number of bytes read.
- */
-ssize_t custom_getline(char **line, size_t *len, FILE *stream)
-{
-	char *buffer = NULL;
-	size_t bufsize = 0;
-	ssize_t bytesRead;
-
-	if (*line == NULL || *len == 0)
-	{
-		bufsize = 1024;
-		buffer = (char *)malloc(bufsize);
-		if (buffer == NULL)
-		{
-			perror("malloc");
-			exit(EXIT_FAILURE);
-		}
-		*line = buffer;
-		*len = bufsize;
-	}
-	else
-	{
-		buffer = *line;
-		bufsize = *len;
-	}
-	bytesRead = getline(&buffer, &bufsize, stream);
-	if (bytesRead != -1)
-	{
-		if (bytesRead > 0 && buffer[bytesRead - 1] == '\n')
-		{
-			buffer[bytesRead - 1] = '\0';
-			bytesRead--;
-		}
-	}
-	else
-	{
-		free(buffer);
-		*line = NULL;
-		*len = 0;
-	}
-	return (bytesRead);
-}
-/**
- * @brief Custom implementation of strcat function.
- * @param str1 The first string.
- * @param str2 The second string.
- * @return The concatenated string.
- */
-char *custom_strcat(const char *str1, const char *str2)
-{
-	size_t len1 = strlen(str1);
-	size_t len2 = strlen(str2);
-	char *result = (char *)malloc(len1 + len2 + 1);
-
-	if (result == NULL)
-	{
-		perror("malloc");
+		fprintf(stderr, "%s: command not found\n", command);
 		exit(EXIT_FAILURE);
 	}
-	strcpy(result, str1);
-	strcat(result, str2);
-	return (result);
-}
-/**
- * @brief Main function for the simple shell program.
- * The function takes user input, processes commands,
- * and executes them.
- * @return 0 on successful execution.
+	else
+	{
+	if (WIFEXITED(status))
+	{
+	if (WEXITSTATUS(status) != EXIT_SUCCESS)
+	{
+		fprintf(stderr, "%s: command failed with exit status %d\n", command, WEXITSTATUS(status));
+	}
+	}
+	else if (WIFSIGNALED(status))
+	{
+		fprintf(stderr, "%s: command terminated by signal %d\n", command, WTERMSIG(status));
+	}
+	}
+}/**
+ * @brief Main function for the simple shell.
  */
-int main()
+int main(void)
 {
-	char *input = NULL;
-	size_t MAX_INPUT_SIZE = 1024;
-	size_t argCount = 0;
-	char *token = NULL;
-	char **env = environ;
-	const char *str1 = "simple, ";
-	const char *str2 = "shell";
-	char *invalid = custom_strcat(str1, str2);
-	char *args[MAX_ARGUMENTS];
+	char command[MAX_COMMAND_LENGTH];
 
 	while (1)
 	{
-		printf("$ ");
-		if (custom_getline(&input, &MAX_INPUT_SIZE, stdin) == -1)
+		displayPrompt();
+		if (fgets(command, MAX_COMMAND_LENGTH, stdin) == NULL)
 		{
+			printf("\nGoodbye.\n");
 			break;
 		}
-		token = strtok(input, " \n\t\r\a");
-		while (token != NULL)
-		{
-			args[argCount++] = token;
-			token = strtok(NULL, " \n\t\r\a");
-		}
-		args[argCount] = NULL;
-		if (argCount > 0)
-		{
-			if (strcmp(args[0], "exit") == 0)
-			{
-				break;
-			}
-			else if (strcmp(args[0], "env") == 0)
-			{
-				while (*env)
-				{
-					printf("%s\n", *env);
-					env++;
-				}
-			}
-			else
-			{
-				executeCommand(args);
-			}
-		}
-		printf("invalid input: %s\n", invalid);
+		command[strcspn(command, "\n")] = '\0';
+		executeCommand(command);
 	}
-	free(invalid);
-	printf("Goodbye.\n");
 	return (0);
 }
+
--- shell.h
+++ shell.h
@@ -1,27 +1,14 @@
 #ifndef SHELL_H
 #define SHELL_H
-#include <stdlib.h>
+
 #include <stdio.h>
-#include <string.h>
-#include <sys/wait.h>
-#include <dirent.h>
-#include <signal.h>
+#include <stdlib.h>
 #include <unistd.h>
-#include <fcntl.h>
+#include <sys/wait.h>
 
-/* global variables */
-extern char **environ;
+#define MAX_COMMAND_LENGTH 1024
 
-void signal_handler(int signal);
-char *our_strcpy(char *dest, const char *src);
-char *strdup(const char *str);
-size_t our_strlen(const char *str);
-int our_strcmp(const char *string1, const char *string2);
-size_t strlen(const char *str);
-ssize_t custom_getline(char **line, size_t *len, FILE *stream);
-void executeCommand(char *args[]);
-void executeCommand(char *args[]);
-char *custom_strcat(const char *str1, const char *str2);
-int main();
+void displayPrompt(void);
+void executeCommand(const char *command);
 
-#endif
+#endif /* SHELL_H */
